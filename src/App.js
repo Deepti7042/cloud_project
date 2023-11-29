4e6925f8f8fc5d77c3e2f2ccb1b9d5fe15787e4a
Deepti <deeptituteja1911@gmail.com>
2023-11-29 15:47:22 -0400

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routes from './Routes';
 import './css/App.css';  // Assuming you have created this CSS file
+import Login from './components/Login';
 
 const App = () => {
-    return <Routes />;
+    // return <Routes />;
+    return <Login/>
 };
 
 export default App;
